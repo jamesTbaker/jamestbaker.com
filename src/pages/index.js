bd9e1ad261e51c2b289953a55e6af4ced2b9e047
jamesTbaker <jamesTbaker@outlook.com>
2019-02-20 19:03:15 -0500

    Dev phase

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,37 +34,39 @@ export default () => (
 			<title>Profile</title>
 			<meta name="description" content="Profile of James T. Baker" />
 		</Helmet>
-		<BodyContentContainer>
-			<p>Greater Boston, USA</p>
-			<p><a href="/Profile Brief - James T. Baker.pdf">Profile Brief</a></p>
-			<p>I speak suit, geek, and creative.</p>
-			<p>
-				My <StrongText>19 years'</StrongText> experience in <StrongText>design</StrongText>,&nbsp;
-				<StrongText>technical product development</StrongText>, and <StrongText>management</StrongText> 
-				have allowed me to <StrongText>lead and execute</StrongText> projects and operations 
-				at the intersection of business, software development, and 
-				visual / UX design + digital media.
-			</p>
-			<h2>Business Skills</h2>
-			<ul>
-				<li>Communicating and motivating across organizational levels and boundaries</li>
-				<li>Leadership</li>
-				<li>Analysis, research, design thinking, systems thinking</li>
-				<li>Staff development, team building</li>
-				<Collapsible
-					expandText="Discover more business skills"
-					collapseText="Show fewer business skills"
-				>
-					<li>Active listening</li>
-					<li>Financial management</li>
-					<li>Business analysis</li>
-				</Collapsible>
-			</ul>
-			<h2>Design Skills</h2>
-			<ul>
-				<li>Art direction</li>
-				<li>A/B testing</li>
-			</ul>
-		</BodyContentContainer>
+		<div>
+			<BodyContentContainer>
+				<p>Greater Boston, USA</p>
+				<p><a href="/Profile Brief - James T. Baker.pdf">Profile Brief</a></p>
+				<p>I speak suit, geek, and creative.</p>
+				<p>
+					My <StrongText>19 years'</StrongText> experience in <StrongText>design</StrongText>,&nbsp;
+					<StrongText>technical product development</StrongText>, and <StrongText>management</StrongText> 
+					have allowed me to <StrongText>lead and execute</StrongText> projects and operations 
+					at the intersection of business, software development, and 
+					visual / UX design + digital media.
+				</p>
+				<h2>Business Skills</h2>
+				<ul>
+					<li>Communicating and motivating across organizational levels and boundaries</li>
+					<li>Leadership</li>
+					<li>Analysis, research, design thinking, systems thinking</li>
+					<li>Staff development, team building</li>
+					<Collapsible
+						expandText="Discover more business skills"
+						collapseText="Show fewer business skills"
+					>
+						<li>Active listening</li>
+						<li>Financial management</li>
+						<li>Business analysis</li>
+					</Collapsible>
+				</ul>
+				<h2>Design Skills</h2>
+				<ul>
+					<li>Art direction</li>
+					<li>A/B testing</li>
+				</ul>
+			</BodyContentContainer>
+		</div>
 	</Container>
 );
